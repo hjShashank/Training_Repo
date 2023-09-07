3c78377343429152aa37224dad76f6200994b7f7
hjShashank <144328048+hjShashank@users.noreply.github.com>
2023-09-07 14:49:01 +0530

    hhhh
    ssss

---
+++ GEN_Application_View.xaml.cs
@@ -0,0 +1,46 @@
+/*********************************************************************************
+**                   AVIN SYSTEMS PVT LTD                                       **
+**********************************************************************************
+** Copyright (C) AVIN SYSTEMS PVT LTD 2021 - All Rights Reserved                **
+** Reproduction, distribution and utilization of this document as well          **
+** as the communication of its contents to others without explicit              **
+** authorization is prohibited.                                                 **
+** Offenders will be held liable for the payment of damages.                    **
+**********************************************************************************
+**  File Name   : GEN_Application_View.xaml.cs										**
+**                                                                              **
+**  PURPOSE     : Determines the starting point of entire code.					**
+**                                                                              **
+**********************************************************************************/
+
+/*********************************************************************************
+** Change History                                                               **
+**********************************************************************************
+ 
+ Revision: 1.0.0     |Date: 13-Jul-2021         |Author: Hardhik Yadav Isarapu
+ - Initial version
+ 
+**********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AVIN_FS_Tool
+{
+    /// <summary>
+    /// Class Name         : GEN_Application_View
+    /// Class Description  : 
+    /// </summary>
+    public partial class GEN_Application_View : Application
+    {
+
+    } //End of public partial class GEN_Application_View : Application
+} //End of namespace AVIN_FS_Tool
+
+/**********************************************************************************
+**                           End of File                                         **
+***********************************************************************************/
